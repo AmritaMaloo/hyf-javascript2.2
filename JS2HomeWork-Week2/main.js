6ad08c4408007f6e4179d77c5e778467aabef892
AmritaMaloo <amrita.maloo@gmail.com>
2018-04-23 14:51:47 +0200

    Step1

---
+++ JS2HomeWork-Week2/main.js
@@ -0,0 +1,5 @@
+ console.log("script " + document.querySelector("script").src +  " has been loaded successfully");
+        
+   
+/*document.querySelector("button").addEventListener('click',  ()  =>  console.log('you clicked me'));*/
+   
