24c5abc59df4bbd755ea0feb898a2e8fee678421
AmritaMaloo <amrita.maloo@gmail.com>
2018-04-24 02:13:55 +0200

    Step 3 added

--- JS2HomeWork-Week2/main.js
+++ JS2HomeWork-Week2/main.js
@@ -1,6 +1,20 @@
-// Step 1 
+// Step 1 Create a new website with external js file
 console.log("script " + document.querySelector("script").src +  " has been loaded successfully");
  
-// Step 2 
+// Step 2 Add a button (e.g. 'click me') that when clicked console.logs 'you clicked me!
 document.querySelector("button").addEventListener('click',  ()  =>  console.log('you clicked me!!'));
-   
+
+// Step 3 Create a function that fetches from The Github API
+function fetchJSONData(url, callbackFn) {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function() {
+        console.log("Data loaded.");
+        const data = JSON.parse(xhr.responseText);
+        callbackFn(data);
+    });
+    xhr.open('GET', url);
+    xhr.send();
+}
+fetchJSONData('https://api.github.com/orgs/HackYourFuture/repos', function(data) {
+   console.log(data); 
+});
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Step 1 added
-Step 2 added:
+Step 2 added
+Step 3 added
